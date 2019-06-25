af63b0ca39e30cf2955d0cf15cdd60267bfd0df9
Milos Kapovic <miloskapovic@gmail.com>
2019-06-25 17:09:39 +0200

    widget da prikazuje produkte

--- src/widget/containers/Widget.js
+++ src/widget/containers/Widget.js
@@ -2,21 +2,29 @@ import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
 const Widget = () => {
-  let Products;
-  buildfire.publicData.get('Products', (err, data) => {
+  let products;
+  buildfire.publicData.search({}, 'Products', (err, data) => {
     if (err) {
       alert('there was a problem retrieving your data');
     } else {
-      Products = data;
-      console.log('got your data ', JSON.stringify(data));
+      products = data;
+      console.log('products>>>>>>', data);
     }
-});
+  });
+
+  const productList = products && products.map(product => (
+    <div>
+      <h1>{product.data.name}</h1>
+      <button type="button" className="btn btn-primary">Order</button>
+    </div>
+  ));
+
   const content = (
     <div>
       <h1>Content - Hello World! - milos</h1>
-      <h1>Products: {Products}</h1>
+      { productList }
     </div>
   );
   return content;
-}
+};
 export default hot(Widget);
