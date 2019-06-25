78ade0004e14e36472593fb1918b9485e0eb67dd
Milos Kapovic <miloskapovic@gmail.com>
2019-06-25 15:12:25 +0200

    Unosim producte preko content prozora u publicData

--- src/control/content/containers/Content.js
+++ src/control/content/containers/Content.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const Content = () => (
-  <h1>Content - Hello World!</h1>
-);
+const Content = () => {
+  const [product, setProduct] = useState(null);
+
+  const handleChange = (e) => {
+    setProduct(e.target.value);
+  };
+
+  const saveProduct = (input) => {
+    buildfire.publicData.save({ name: input }, 'Products', (err, data) => {
+      if (err) {
+        alert('there was a problem saving your data');
+      } else {
+        console.log('your data has saved successfully', data);
+      }
+    });
+  };
+
+  const getData = () => {
+    let products;
+    buildfire.publicData.get('Products', (err, data) => {
+      if (err) {
+        alert('there was a problem retrieving your data');
+      } else {
+        products = data;
+        console.log('got your data ', data);
+      }
+    });
+    console.log('products', products);
+  };
+
+  const content = (
+    <div>
+      <h1>Unesi Produkt: </h1>
+      <input type="text" onChange={handleChange} />
+      <button type="button" className="btn btn-primary" onClick={() => saveProduct(product)}>Save</button>
+      <button type="button" className="btn btn-primary" onClick={() => getData()}>getData</button>
+    </div>
+  );
+  return content;
+};
 
 export default hot(Content);
--- src/widget/containers/Widget.js
+++ src/widget/containers/Widget.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 
-const Widget = () => (
-  <h1>Widget - Hello World!</h1>
-);
-
+const Widget = () => {
+  let Products;
+  buildfire.publicData.get('Products', (err, data) => {
+    if (err) {
+      alert('there was a problem retrieving your data');
+    } else {
+      Products = data;
+      console.log('got your data ', JSON.stringify(data));
+    }
+});
+  const content = (
+    <div>
+      <h1>Content - Hello World! - milos</h1>
+      <h1>Products: {Products}</h1>
+    </div>
+  );
+  return content;
+}
 export default hot(Widget);
